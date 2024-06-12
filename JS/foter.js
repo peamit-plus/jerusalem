fee338043535930828330383e0cde5a603486354
peamit-plus <a0503436201@gmail.com>
2024-06-12 21:59:20 +0300

    css,html,js

--- JS/foter.js
+++ JS/foter.js
@@ -4,7 +4,10 @@ document.getElementById("footer").innerHTML=`
 
     <div class="container" style="background-color: #333;">
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
+          <img src="https://peamit-plus.github.io/jerusalem/image/rel.png"  />
+          </div>
+          <div class="col-md-3">
                 <h3>יצירת קשר</h3>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSe12UgtnTNi8pcSFSZ09V3OCWxKMmXCTzOMylstH7djGJXPVQ/viewform" target="_blank">דיווח על תקלות </a><br/>
                  <a href="tel:+972556610747">בפלאפון 0556610747</a><br/>
@@ -12,14 +15,14 @@ document.getElementById("footer").innerHTML=`
                  <a href="https://g.page/r/CSWWTVep_ek9EBM/review" target="_blank">מתן ביקורת לפעמית פלוס + ⭐</a>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
               <h3>המוצרים שלנו</h3>
                 <a href="#category1">צפיה בקטלוג</a><br/ >
                 <a href="./search.html">חיפוש מוצר 👌</a><br/>
                 <a href="../page/products/index1.html">התחלת קניה</a><br/>
                 <!-- <a href="#"  onclick="notfound()"></a>-->
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
               <h3>פעמית פלוס ירושלים</h3>
                   <a href="../">דף הבית</a><br/>
                   <a href="./about.html">אדותינו</a><br/>
