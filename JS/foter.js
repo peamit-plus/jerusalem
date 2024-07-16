532f442b50d0bf5f2e94125dfe3a105fc585be23
peamit-plus <a0503436201@gmail.com>
2024-07-16 20:11:11 +0300

    footer

--- JS/foter.js
+++ JS/foter.js
@@ -2,38 +2,53 @@
 function start() {
     document.getElementById("footer").innerHTML = `
 
-    <div class="container" style="background-color: #333;">
-        <div class="row" dir="rtl">
-
-        <div class="col-md-3" dir="rtl">
-              <h3>פעמית פלוס ירושלים</h3>
-                  <a href="../">דף הבית</a><br/>
-                  <a href="./about.html">אדותינו</a><br/>
-                  <a href="./help.html">עזרה</a>
-          </div>
-          <div class="col-md-3" dir="rtl">
-              <h3>המוצרים שלנו</h3>
-                <a href="#category1">צפיה בקטלוג</a><br/ >
-                <a href="./search.html">חיפוש מוצר 👌</a><br/>
-                <a href="../page/products/index1.html">התחלת קניה</a><br/>
-                <!-- <a href="#"  onclick="notfound()"></a> -->
-          </div>
-         <div class="col-md-3" dir="rtl">
-                <h3>יצירת קשר</h3>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSe12UgtnTNi8pcSFSZ09V3OCWxKMmXCTzOMylstH7djGJXPVQ/viewform" target="_blank">דיווח על תקלות </a><br/>
-                 <a href="tel:+972556610747">בפלאפון 0556610747</a><br/>
-                 <a href="mailto:avizeev85@gmail.com">avizeev85@gmail.com</a><br/>
-                 <a href="https://g.page/r/CSWWTVep_ek9EBM/review" target="_blank">מתן ביקורת לפעמית פלוס + ⭐</a>
-          </div>
-          <div class="col-md-3 img-center" dir="rtl">
-          <img src="https://peamit-plus.github.io/jerusalem/image/rel.png"  style="width: 120px;  background: rgba(255, 255, 255, 0);" class="" alt="Image"> />
-          </div>
-          </div>
-    <hr/ class="w-100">
-  
-    <p class="text-center"> ט.ל.ח © כל הזכויות שמורות לפעמית פלוס ירושלים 2023-2024</p>
+<div class="container" style="background-color: #333;">
+    <div class="row rtl">
+      <div class="col-md-3">
+        <h3>פעמית פלוס ירושלים</h3>
+        <a href="../">דף הבית</a><br/>
+        <a href="./about.html">אדותינו</a><br/>
+        <a href="./help.html">עזרה</a>
+      </div>
+      <div class="col-md-3">
+        <h3>המוצרים שלנו</h3>
+        <a href="#category1">צפיה בקטלוג</a><br/>
+        <a href="./search.html">חיפוש מוצר 👌</a><br/>
+        <a href="../page/products/index1.html">התחלת קניה</a><br/>
+      </div>
+      <div class="col-md-3">
+        <h3>יצירת קשר</h3>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSe12UgtnTNi8pcSFSZ09V3OCWxKMmXCTzOMylstH7djGJXPVQ/viewform" target="_blank">דיווח על תקלות </a><br/>
+        <a href="tel:+972556610747">בפלאפון 0556610747</a><br/>
+        <a href="mailto:avizeev85@gmail.com">avizeev85@gmail.com</a><br/>
+        <a href="https://g.page/r/CSWWTVep_ek9EBM/review" target="_blank">מתן ביקורת לפעמית פלוס + ⭐</a>
+      </div>
+      <div class="col-md-3 img-center">
+        <img src="https://peamit-plus.github.io/jerusalem/image/rel.png" style="width: 120px; background: rgba(255, 255, 255, 0);" class="" alt="Image"/>
+      </div>
     </div>
+    <hr class="w-100">
+    <p class="text-center"> ט.ל.ח © כל הזכויות שמורות לפעמית פלוס ירושלים 2023-2024</p>
+  </div>
 `}
 start()
 
-
+// function footer() {
+//     var footer = `
+//         <div class="container">
+//             <row>
+//                 <div class="col-md-3>
+//                     <h3>col 1</h3>
+//                     <a hre></a>
+//                 </div>
+//                 <div class="col-md-3>
+//                     <h3>col 2</h3>
+//                 </div>
+//                 <div class="col-md-3>
+//                      <h3>col 3</h3>
+//                 </div>
+//             </row>
+//         </div>
+//  `
+// }
+// footer()
