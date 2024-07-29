a13a38576ed93b5dcd57b2a712fddec45b7e5a9a
peamit-plus <a0503436201@gmail.com>
2024-07-29 11:43:54 +0300

    js

--- JS/Catergory.js
+++ JS/Catergory.js
@@ -8,7 +8,8 @@ function createCategory(json) {
        <img src='../imgctg/${element.image}.WebP' alt='${element.p}' style='width:100%;height:270px;' class='carsor' onclick='index${element.image}()'>
          <div class='caption'>
             <p class='text-primary'>${element.p}<br/>
-            <button class='btn btn-primary' onclick='index${element.image}()'>&laquo; לקטגוריה לחץ</button> </p>
+            <a class="btn btn-primary" href="../products/index${element.image}.html">&laquo; לקטגוריה לחץ</a> </p>
+    
       </div>
     </div>
     </div>
