a5e4a63d1c37c43e155a3a01535f717fa3a82deb
peamit-plus <a0503436201@gmail.com>
2024-08-07 10:50:24 +0300

    js

--- JS/clculate.js
+++ JS/clculate.js
@@ -12,32 +12,35 @@ function displayProductsFromLocalStorage() {
     let totalProductPrice = product.price * (product.quantity || 1);
 
     let productCard = `
-            <div class="col-md-4 product-card" id="product-${key}">
-                <div class="panel panel-info">
-                    <div class="panel-heading text-center">
-                        <h3>${product.descraption}</h3>
-                    </div>
-                  <div class="panel-body text-center">
-                  <h4 id="productPrice-${key}">${totalProductPrice}₪</h4>
-                     <div class="btn-group">
-                      <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', -1)">-</button>
-                      <button type="button" class="btn btn-info"><span id="quantity-${key}">${product.quantity || 1}</span></button>
-                      <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', 1)">+</button>
-                    </div>
-                  </div>
-                    <div class="panel-body text-center">
-                        <input type="text" dir="rtl" class="form-control" value="${product.color || 'צבע ברירת מחדל (מה שקיים במלאי) '}" onchange="updateColor('${key}', this)">
-                    </div>
-
-                    <div class="panel-footer text-center" style="background:#d9edf7;">
-                        
-                        <button  class="button" onclick="deleteProduct('${key}')">
-                        <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
-                      </button>
-                    </div>
+          <div class="col-md-4 product-card" id="product-${key}">
+    <div class="panel panel-primary">
+        <div class="panel-heading text-center">
+            <h3>${product.descraption}</h3>
+        </div>
+        <div class="panel-body">
+            <div class="text-center">
+                <img src="${product.image || 'default-image.jpg'}" alt="${product.descraption}" class="img-responsive center-block" style="max-height: 200px; margin-bottom: 10px;">
+            </div>
+            <h4 class="text-center" id="productPrice-${key}">${totalProductPrice}₪</h4>
+            <div class="text-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', -1)">-</button>
+                    <button type="button" class="btn btn-info"><span id="quantity-${key}">${product.quantity || 1}</span></button>
+                    <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', 1)">+</button>
                 </div>
             </div>
-        `;
+            <div class="text-center" style="margin-top: 10px;">
+                <input type="text" dir="rtl" class="form-control" value="${product.color || 'צבע ברירת מחדל (מה שקיים במלאי) '}" onchange="updateColor('${key}', this)">
+            </div>
+        </div>
+        <div class="panel-footer text-center" style="background-color: #f5f5f5;">
+            <button class="btn btn-danger" onclick="deleteProduct('${key}')">
+                <span class="glyphicon glyphicon-trash"></span>
+            </button>
+        </div>
+    </div>
+</div>
+ `;
 
     productsContainer.innerHTML += productCard;
 
