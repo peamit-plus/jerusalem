9faa02a7f2760b1746b7c3b2ef4cd797cf70d257
peamit-plus <a0503436201@gmail.com>
2024-08-07 12:04:28 +0300

    js

--- JS/clculate.js
+++ JS/clculate.js
@@ -11,35 +11,37 @@ function displayProductsFromLocalStorage() {
 
     let totalProductPrice = product.price * (product.quantity || 1);
 
-    let productCard = `<div class="col-md-4 product-card" id="product-${key}">
-    <div class="panel panel-default">
-        <div class="panel-heading text-center">
-            <h3>${product.descraption}</h3>
-        </div>
-        <div class="panel-body text-center">
-            <h4 id="productPrice-${key}">${totalProductPrice}₪</h4>
-            <div class="btn-group" style="margin-bottom: 10px;">
-                <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', -1)">-</button>
-                <button type="button" class="btn btn-info"><span id="quantity-${key}">${product.quantity || 1}</span></button>
-                <button type="button" class="btn btn-default" onclick="updateQuantity('${key}', 1)">+</button>
-            </div>
-            <div style="margin-top: 10px;">
-                <input type="text" dir="rtl" class="form-control" value="${product.color || 'צבע ברירת מחדל (מה שקיים במלאי) '}" onchange="updateColor('${key}', this)">
-            </div>
-        </div>
-        <div class="panel-footer text-center">
-            <button class="btn btn-danger" onclick="deleteProduct('${key}')">
-                <span class="glyphicon glyphicon-trash"></span> מחק מוצר
-            </button>
-        </div>
-    </div>
-</div>
-`;
+    let productCard = `
+            <div class="col-md-4 product-card" id="product-${key}">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-center">
+                        <h3>${product.descraption}</h3>
+                    </div>
+                    <div class="panel-body text-center">
+      <div class="btn-group">
+    <button type="button" class="btn btn-danger" onclick="updateQuantity('${key}', -1)">-</button>
+    <button type="button" class="btn "><span id="quantity-${key}">${product.quantity || 1}</span></button>
+    <button type="button" class="btn btn-success" onclick="updateQuantity('${key}', 1)">+</button>
+  </div>
+      </div>
+                    <div class="panel-body text-center">
+                        <input type="text" dir="rtl" class="form-control" value="${product.color || 'צבע ברירת מחדל (מה שקיים במלאי) '}" onchange="updateColor('${key}', this)">
+                    </div>
 
-    productsContainer.insertAdjacentHTML('beforeend', productCard);
+                    <div class="panel-footer text-center" style="background:#d9edf7;">
+                        <h4 id="productPrice-${key}">${totalProductPrice}₪</h4>
+                        <button  class="button" onclick="deleteProduct('${key}')">
+                        <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
+                      </button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+    productsContainer.innerHTML += productCard;
+
   }
 }
-
 function calculateTotalPrice() {
   var totalPrice = 0;
   var totalProduct = 0;
@@ -51,13 +53,15 @@ function calculateTotalPrice() {
   for (let i = 0; i < localStorage.length; i++) {
     let product = JSON.parse(localStorage.getItem(localStorage.key(i)));
     totalProduct += ((product.quantity || 1));
+    
   }
 
   var total;
   if (totalPrice == 0) {
     total = `<h4 class="text-center" style="color:red;">אין מוצרים בסל<h4/>`
-  } else {
-    total = `סה"כ: ${totalPrice}₪ <br/> סה"כ מוצרים בסל : ${totalProduct}`;
+  }
+  else {
+    total = `סה"כ: ${totalPrice}₪ <br/> סה"כ מוצרים בסל : ${totalProduct}`
   }
 
   document.getElementById('totalPrice').innerHTML = total;
@@ -65,7 +69,7 @@ function calculateTotalPrice() {
 
 function updateQuantity(key, amount) {
   let product = JSON.parse(localStorage.getItem(key));
-  let newQuantity = (product.quantity || 1) + amount;
+  let newQuantity = (product.quantity || 0) + amount;
 
   // בדיקה שהכמות איננה שלילית
   if (newQuantity < 1) {
@@ -79,7 +83,7 @@ function updateQuantity(key, amount) {
 
   localStorage.setItem(key, JSON.stringify(product));
   document.getElementById(`quantity-${key}`).innerText = product.quantity;
-  
+
   calculateTotalPrice();
 }
 
@@ -90,27 +94,31 @@ function deleteProduct(key) {
   var confirmation = confirm("האם אתה בטוח שברצונך למחוק מוצר זה מסל הקניות ?");
 
   // בדיקה אם המשתמש אישר את הפעולה
-  if (confirmation) {
-    confirmDeleteProduct(key);
-    calculateTotalPrice();
+  if (confirmation) {// הפעולה לאישור
+    confirmDeleteProduct(`${key}`)
   }
-}
+  else {
+    return
+  }
 
+}
 function closeM() {
   const modal = document.querySelector('.modal');
   modal.remove();
 }
 
 function confirmDeleteProduct(key) {
-  console.log("Deleting product with key:", key);
+  console.log("B", key)
   let productCard = document.getElementById(`product-${key}`);
   productCard.classList.add('fade-out');
 
   setTimeout(() => {
     localStorage.removeItem(key);
     productCard.remove();
+    calculateTotalPrice();
   }, 500); // Adjust the time based on your animation duration
 }
+
 
 function copyUpdatedData() {
   if (!confirm(" לידיעתך ! \n אם אתה ממשיך לשלב הבא לא תוכל לחזור שוב ולעדכן את ההזמנה בדוק את עצמך שוב \nהאם אתה רוצה להמשיך לשלב הבא ?")) {
@@ -136,3 +144,5 @@ function copyUpdatedData() {
 
   window.location.href = completeURL;
 }
+
+
