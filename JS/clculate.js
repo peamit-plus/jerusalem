8da0afa425634619aa4de8f1e2375f6e25d76a5b
peamit-plus <a0503436201@gmail.com>
2024-08-07 16:58:50 +0300

    cs

--- JS/clculate.js
+++ JS/clculate.js
@@ -30,7 +30,7 @@ function displayProductsFromLocalStorage() {
 
                     <div class="text-center">
                         <h4 class="text-primary" id="productPrice-${key}">${totalProductPrice}₪</h4>
-                        <button class=" btn btn-defult deleteProductInEnd" onclick="deleteProduct('${key}')">
+                        <button class="deleteProductInEnd" onclick="deleteProduct('${key}')">
                         <span class="glyphicon glyphicon-trash"></span>
                         </button>
                     </div>
