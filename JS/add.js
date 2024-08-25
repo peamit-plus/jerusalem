881ee6f4b741783449fa6caafc45402588aa44c6
peamit-plus <a0503436201@gmail.com>
2024-08-25 14:45:43 +0300

    css

--- JS/add.js
+++ JS/add.js
@@ -1224,7 +1224,7 @@ const productsDB = {
         { "image": "11", "p": "היגיינה וטיפוח" },
         { "image": "12", "p": "שונות - חומרי ניקוי" },
     ],
-    modal:`"
+    modal:`
     <div class='modal fade' id='myModal' role='dialog'>
         <div class='modal-dialog modal-lg'>
           <div class='modal-content'>
@@ -1247,7 +1247,7 @@ const productsDB = {
         </div>
         <div class='modal-footer'>
         <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-        </div></div></div></div>"`
+        </div></div></div></div>`
 }
 
 
